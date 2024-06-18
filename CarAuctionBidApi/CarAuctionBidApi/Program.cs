8460c5395af05cdc6919c1705dfa242d8a2b72e8
Johny Georges <jgeorges@spiria.com>
2024-06-18 18:04:03 -0400

    added cors policy in API (allowed any origin to get it working. Might re-visit tomorrow).

--- CarAuctionBidApi/CarAuctionBidApi/Program.cs
+++ CarAuctionBidApi/CarAuctionBidApi/Program.cs
@@ -1,6 +1,14 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("localhostPolicy",
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("localhostPolicy");
 
 app.UseHttpsRedirection();
 
